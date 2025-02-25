7b77c4d129ab5c1188c093d51416ec915c04da0e
Glcharan <135237836+Glcharan@users.noreply.github.com>
2025-02-25 12:18:36 +0530

    Delete Assignment0_23017 directory

--- Assignment0_23017/Assignment0_23017/Q4.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-Matrix = np.zeros((4,3))
-print("Matrix of zeros with 4 row and 3 columns:\n",Matrix)
-Matrix = np.random.randint(1,100,size=(4,3))
-array = Matrix.flatten()
-
-print("Matrix filled with Random Numbers:\n",Matrix)
-print("single dimensional array of the Matrix:\n",array)
